b15ee187c7e59ca7e1ec741b79f8b8d6966cc704
Anders Björkland <git@andersbjorkland.online>
2021-02-11 22:54:59 +0100

    added forgot password frame

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,8 +1,20 @@
 import { CenteredContent, Container, Wrapper } from "./PageLayout";
 import Login from "../Components/Login";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import GenericButton from "../Components/UIButtons/GenericButton";
+
+const TARGET_URL = window.location.hostname === 'localhost' ? process.env.REACT_APP_ROOT_URL_DEV : process.env.REACT_APP_ROOT_URL;
+
+const resetFrame = (
+  <div className="flex-column">
+    <iframe title="reset-password" src={TARGET_URL + "/reset-password"} frameborder="0">...loading</iframe>
+  </div>
+);
 
 const LoginPage = (props) => {
+
+  const [forgotten, setForgotten] = useState(false);
    
     return (
         <Wrapper>
@@ -12,6 +24,10 @@ const LoginPage = (props) => {
               <Login props={props} />
               <p>New user? <Link to="/register">Register</Link></p>
             </CenteredContent>
+            <div className="flex-column">
+              <GenericButton className="mr-auto" onClick={() => setForgotten(!forgotten)}>Forgot password?</GenericButton>
+              {forgotten && resetFrame}
+            </div>
           </Container>
         </Wrapper>
     );
